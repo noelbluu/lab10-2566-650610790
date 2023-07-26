53d101b71680a1bf407cf4adf9a7c2c3facbfe8b
noelbluu <panida.stp@icloud.com>
2023-07-26 13:13:39 +0700

    update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
 "use client";
 
+import { UserCard } from "@/components/UserCard";
+import { cleanUser } from "@/libs/cleanUser";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function RandomUserPage() {
-  //user = null or array of object
   const [users, setUsers] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const [genAmount, setGenAmount] = useState(1);
+  const [genAmount, setGenAmount] = useState(0);
+  const [isFirstLoad, setFirstLoad] = useState(true);
 
   const generateBtnOnClick = async () => {
     setIsLoading(true);
@@ -16,10 +18,26 @@ export default function RandomUserPage() {
     );
     setIsLoading(false);
     const users = resp.data.results;
-    //Your code here
-    //Process result from api response with map function. Tips use function from /src/libs/cleanUser
-    //Then update state with function : setUsers(...)
+    const cleanedUsers = users.map((person) => cleanUser(person));
+    setUsers(cleanedUsers);
   };
+
+  useEffect(() => {
+    if (isFirstLoad) {
+      setFirstLoad(false);
+      return;
+    }
+    localStorage.setItem("genAmount", genAmount);
+  }, [genAmount]);
+
+  useEffect(() => {
+    const genAmount = localStorage.getItem("genAmount");
+    if (genAmount == null || genAmount == "") {
+      setGenAmount(1);
+      localStorage.setItem("genAmount", 1);
+      return;
+    }
+  }, []);
 
   return (
     <div style={{ maxWidth: "700px" }} className="mx-auto">
@@ -40,7 +58,16 @@ export default function RandomUserPage() {
       {isLoading && (
         <p className="display-6 text-center fst-italic my-4">Loading ...</p>
       )}
-      {users && !isLoading && users.map(/*code map rendering UserCard here */)}
+      {users &&
+        !isLoading &&
+        users.map((users) => (<UserCard
+          key={users.email}
+          name={users.name}
+          email={users.email}
+          imgUrl={users.imgUrl}
+          address={users.address}
+        />
+        ))}
     </div>
   );
 }
